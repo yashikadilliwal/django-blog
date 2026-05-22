dd50460c4cab551a5c798851dc6599479ffc6788
yashikadilliwal <yashiikpop@gmail.com>
2026-05-22 14:04:28 +0530

    fix;fixed crispy forms

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
